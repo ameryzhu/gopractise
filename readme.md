ebec03e50d8fd8895f2549322549fbb452d3d618
mason <ameryzhu@qq.com>
2021-09-22 23:44:28 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,2 @@
+# follow website
+https://www.runoob.com/go/go-variables.html
---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+func main(){
+	basic()
+	shortForm()
+}
+
+func basic(){
+	var a int = 1
+	var b string = "b"
+	var c bool = true
+	print(a,b,c)
+}
+
+func shortForm(){
+	a:=1
+	print(a)
+}
